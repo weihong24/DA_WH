47b46538ec9d2ebd2e35ca5c936c420e0c65c330
Weihong <tan_wei_hong4@connect.ite.edu.sg>
2020-02-28 17:04:43 +0800

    initial creation

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 class DA:
      def read_data(self):
-        file = 'data.csv'
+        file = 'Worksheet in D  Lesson 2019 Applied Sriptong Using Python Python Elective (IT49450) - Project_14Mar19.csv'
         df = pd.read_csv(file)
         print(df)
 
@@ -14,3 +14,8 @@ class DA:
 
 regeion = DA()
 regeion.read_data()
+
+import matplotlib.pyplot as plt
+
+year = [2008,2009,2010,2011,2012,2013,2014,2015,2016,2017]
+regeion = []
