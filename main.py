7681d28bd9b5ea21f583794bfe5160887bd131f3
Weihong24 <tan_wei_hong4@connect.ite.edu.sg>
2020-03-02 16:21:31 +0800

    filter table

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.pyplot as plt
+import unittest
+
 
 
 class DA:
@@ -10,7 +12,12 @@ class DA:
         file = 'Worksheet in D  Lesson 2019 Applied Sriptong Using Python Python Elective (IT49450) - Project_14Mar19.csv'
         df = pd.read_csv(file)
         print(df)
-        print(df.iloc[360:480,[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]])
+        #filter table
+        print(df.iloc[360:480,[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]])
+
+
+
+
 
 
 
